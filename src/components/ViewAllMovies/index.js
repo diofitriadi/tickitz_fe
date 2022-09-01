0eccdeeae6546bb9777eb91607e69c18da44cce0
Dio Fitriadi <fitriadidio@gmail.com>
2022-09-02 03:54:12 +0700

    feat: "add pagination"

--- src/components/ViewAllMovies/index.js
+++ src/components/ViewAllMovies/index.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import ReactPaginate from 'react-paginate'
+import './style.css'
 
 
 
@@ -11,6 +13,7 @@ const ViewMovies = () => {
   const [refetch, setRefetch] = useState(false)
   const [search, setSearch] = useState('title')
   const [query, setQuery] = useState ({})
+  
 
   const [movieDetails, setMovieDetails] = useState({
     loading: 'false',
@@ -36,6 +39,28 @@ const ViewMovies = () => {
     })
   }, [refetch, query])
 
+  //pagination
+  const [pageNumber, setPageNumber] = useState(0)
+    const detailsPerPage = 4
+    const pageVisited = pageNumber * detailsPerPage
+    
+    const displayDetails = movieDetails.results.data
+    const displayMovieDetails = displayDetails.slice(pageVisited, pageVisited + detailsPerPage).map((movies, index) => {
+      return(
+        <div className="movies-card d-flex flex-column mt-5 border p-2 justify-content-center align-items-center rounded-3" key={index}>
+          <img className="card-movie-list image-size rounded-3 mt-2" src={`${process.env.REACT_APP_URL_BE}/uploads/${movies.cover}`} alt={movies.title}/>
+          <p className="details-title text-center mt-2 mx-2">{movies.title}</p>
+          <p className="details-categories text-center mx-1">{movies.categories}</p>
+          <p className="details-release text-center">{moment(movies.release_date).format('DD MMMM YYYY')}</p>
+          <button className="rounded-2 button-view-movies"><Link to="/movie-details">Details</Link></button>
+        </div>
+      )
+    })
+    const pageCount = Math.ceil(displayDetails.length / detailsPerPage)
+    const changePage = ({selected}) => {
+      setPageNumber(selected)
+    }
+    
   return (
     <>
       <div className="form-filter m-5 mx-auto px-4 main-parent">
@@ -88,18 +113,19 @@ const ViewMovies = () => {
                 </select>
           </div>
         </div>
-          <div className="cards-movie d-flex flex-wrap justify-content-between">
-            {movieDetails.results.data.map((movies, index) => {
-              return(
-                <div className="card-size card-movie d-flex flex-column mt-5 border p-2 justify-content-center align-items-center rounded-3" key={index}>
-                  <img className="card-movie-list image-size rounded-3 mt-2" src={`${process.env.REACT_APP_URL_BE}/uploads/${movies.cover}`} alt={movies.title}/>
-                  <p className="text-center mt-2 mx-2">{movies.title}</p>
-                  <p className="text-center mx-1">{movies.categories}</p>
-                  <p className="text-center">{moment(movies.release_date).format('DD MMMM YYYY')}</p>
-                  <button className="rounded-2 button-showing"><Link to="/movie-details">Details</Link></button>
-                </div>
-              )
-            })}
+          <div className="cards-movie d-flex flex-wrap justify-content-between movie-details">
+            {displayMovieDetails} 
+            <ReactPaginate
+              previousLabel={"Prev"}
+              nextLabel={"Next"}
+              pageCount={pageCount}
+              onPageChange={changePage}
+              containerClassName={"pagination-btn"}
+              previousLinkClassName={"previous-btn"}
+              nextLinkClassName={"next-btn"}
+              disabledClassName={"pagination-disable"}
+              activeClassName={"pagination-active"}
+            />
           </div>
         </div>
     </>
